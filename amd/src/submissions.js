809f9c41d2d8c93238ae1032ffe145a55bfed437
Mattan Dor <mattan.dor1@gmail.com>
2023-06-27 12:34:13 +0300

    Fixed: grunt eslint warnings

--- amd/src/submissions.js
+++ amd/src/submissions.js
@@ -1,21 +1,21 @@
 /**
- *  * JS functions.
- *  *
- *  * @package    plagiarism_originality
- *  * @copyright  2023 mattandor <mattan@centricapp.co>
- *  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- *  */
+ * JS functions.
+ *
+ * @package
+ * @copyright  2023 mattandor <mattan@centricapp.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-define(['jquery'], function ($) {
+define(['jquery'], function($) {
     return {
-        unsetLimit: function () {
+        unsetLimit: function() {
             var attemptreopenmethod = $('#id_attemptreopenmethod'),
                 maxattempts = $('#id_maxattempts');
 
             attemptreopenmethod.find('option').removeAttr('disabled');
             maxattempts.find('option').removeAttr('disabled');
         },
-        setLimit: function () {
+        setLimit: function() {
             var attemptreopenmethod = $('#id_attemptreopenmethod'),
                 maxattempts = $('#id_maxattempts');
 
@@ -27,7 +27,7 @@ define(['jquery'], function ($) {
             maxattempts.find('[value=2]').prop('selected', true).removeAttr('disabled');
             maxattempts.change().click();
         },
-        submissions: function () {
+        submissions: function() {
 
             var that = this;
             var originalityDropdown = $('#id_originality_use'),
@@ -36,9 +36,9 @@ define(['jquery'], function ($) {
                 originalityStatus = originalityDropdown.val(),
                 allowFilesGhostwriter = '.rtf,.docx,.doc';
 
-            // ghostwriter Init
+            // Ghostwriter Init
             var el = $('#fitem_fgroup_id_assignsubmission_file_filetypes'),
-                ghostinit = setInterval(function () {
+                ghostinit = setInterval(function() {
                     {
                         if (originalityDropdownGhostwriter.val() == 1) {
                             el.find('[type=text]').val(allowFilesGhostwriter).attr('readonly', true);
@@ -50,14 +50,14 @@ define(['jquery'], function ($) {
                     }
                 }, 2000);
 
-            originalityDropdownGhostwriter.change(function () {
+            originalityDropdownGhostwriter.change(function() {
                 var base = $(this),
                     status = base.val();
 
                 if (status == 1) {
                     el.find('[type=text]').val(allowFilesGhostwriter).attr('readonly', true);
                     el.find('[type=button]').attr('disabled', true);
-                    // notify(true);
+                    // Notify(true);
                 } else {
                     el.find('[type=text]').removeAttr('readonly');
                     el.find('#id_assignsubmission_file_filetypes').val('*');
@@ -69,7 +69,7 @@ define(['jquery'], function ($) {
                 that.setLimit();
             }
 
-            originalityDropdown.change(function () {
+            originalityDropdown.change(function() {
                 var base = $(this),
                     status = base.val(),
                     msg = $('#assignment_has_submissions_notifications');
@@ -84,30 +84,30 @@ define(['jquery'], function ($) {
                     $('.originality-message').toggle(true);
 
                     if (msg.length) {
-                        require(['core/notification'], function (notification) {
+                        require(['core/notification'], function(notification) {
                             notification.alert('Notification', msg.html(), 'OK');
                         });
                     }
                 }
             });
-            submitBtn.on('click', function (e) {
+            submitBtn.on('click', function(e) {
                 var isOnlinetext = $('#id_assignsubmission_onlinetext_enabled').is(':checked'),
                     isGwon = $('#id_originality_use_ghostwriter').val();
 
                 if (isOnlinetext && isGwon == 1 && $('.originality-gw-notify').length) {
                     e.preventDefault();
-                    require(['core/notification'], function (notification) {
+                    require(['core/notification'], function(notification) {
                         notification.alert('', $('.core-notification msg').text(), $('.core-notification btn').text());
                     });
                 }
             });
 
             if ($('#originality-checkbox').length) {
-                $('#region-main input[id=id_submitbutton]').click(function (e) {
+                $('#region-main input[id=id_submitbutton]').click(function(e) {
                     var isChecked = $('#originality-checkbox').is(':checked');
                     if (!isChecked) {
                         e.preventDefault();
-                        require(['core/notification'], function (notification) {
+                        require(['core/notification'], function(notification) {
                             notification.alert('Originality Warning',
                                 $('.core-notification msg').text(), $('.core-notification btn').text());
                         });
@@ -115,7 +115,7 @@ define(['jquery'], function ($) {
                     }
                 });
 
-                $('#region-main #originality-checkbox').click(function () {
+                $('#region-main #originality-checkbox').click(function() {
                     var isChecked = $(this).is(':checked');
                     $('.originality-checkbox-form').remove();
                     $('.mform').prepend('<input type="hidden" value="' + isChecked + '" ' +
