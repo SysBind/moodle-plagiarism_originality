2e7e10e6886f32347148b41dfb3991d0e770dabe
Mattan Dor <mattan.dor1@gmail.com>
2024-04-05 11:00:32 +0300

    Added: increase plugin version.

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ if (!during_initial_install()) {
 }
 
 $plugin = new stdClass();
-$plugin->version = 2024031200;
+$plugin->version = 2024040500;
 $plugin->requires = 2016061505;
 $plugin->component = 'plagiarism_originality';
 $plugin->release = '7.1';
